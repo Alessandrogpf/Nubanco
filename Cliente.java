60fc3a89e01e1154400aa669483f33d1f24d7fa0
Alessandro <alessandrogpf@hotmail.com>
2023-05-24 23:53:16 -0400

    Alteração na classe cliente feita pelo Matheus

--- Cliente.java
+++ Cliente.java
@@ -1,30 +1,41 @@
-public class Cliente extends Pessoa {
-	/* ATRIBUTOS */
-	private String numeroCelular;
-	private String endereco;
-	private String login;
-	private String senha;
-	private String email;  
+public class Cliente {
+    /* ATRIBUTOS */
+    private String nome;
+    private String CPF;
+    private String dataNascimento;
+    private int idade;
+    private String numeroCelular;
+    private String endereco;
+    private String login;
+    private String senha;
+    private String email;  
 
-	/* CONSTRUTOR */
-	public Cliente(String nome, String CPF, String dataNascimento, int idade, String numeroCelular, String endereco, String login, String senha, String email){
-		super(nome, CPF, dataNascimento, idade);
-	    this.numeroCelular = numeroCelular;
-	    this.endereco = endereco;
-	    this.login = login;
-	    this.senha = senha;
-	    this.email = email;
-	}
-	
-	/* MÉTODOS */
+    /* CONSTRUTOR */
+    public Cliente(String nome, String CPF, String dataNascimento, int idade, String numeroCelular, String endereco, String login, String senha, String email)
+    {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.dataNascimento = dataNascimento;
+        this.idade = idade;
+        this.numeroCelular = numeroCelular;
+        this.endereco = endereco;
+        this.login = login;
+        this.senha = senha;
+        this.email = email;
+    }
+    
+    /* MÉTODOS */
 
-	String idadeMinima(int idade){
-		if(idade < 18){
-			return "Você não atende aos requisitos mínimos de idade para uso dos nossos serviços.";
-		} else {
-			return "Tudo certo!";
-		}
-	}
-	
+    /* POSSÍVELMENTE TRANSFORMAR EM EXCESSÃO!
+     * 
+    String idadeMinima(int idade){
+        if(idade < 18){
+            return "Você não atende aos requisitos mínimos de idade para uso dos nossos serviços.";
+        } else {
+            return "Tudo certo!";
+        }
+    }
+    
+    */
 
 }
