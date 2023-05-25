123ea23deb3979bc572497421e447cdcaf61ee37
Alessandro <alessandrogpf@hotmail.com>
2023-05-25 00:11:38 -0400

    Adicionada algumas classes para analise, instancias na main

---
+++ Banco.java
@@ -0,0 +1,7 @@
+ /* Possível Classe para armazenar os clientes e cálculos do sistema */
+ public class Banco { // Ou public class Sistema
+    private pessoaJuridica clientesJ[];
+    private pessoaFisica clientesF[];
+  
+   
+  }
