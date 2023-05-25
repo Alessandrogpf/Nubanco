464be3051cdec3d2b87c4b3c9dbb916d4a6a18d8
Alessandro <alessandrogpf@hotmail.com>
2023-05-24 23:55:52 -0400

    Alteração na classe Conta

--- Conta.java
+++ Conta.java
@@ -1,15 +1,18 @@
 public class Conta {
     /* ATRIBUTOS */
     private double saldo;
-    private double limCredito; // Limite crédito
-    private double limDiarioDebito; // Limite diário débito
+    private double numConta;
+    private double numAgencia;
+    
   
     //construtor
-    Conta(){
-      
+    public Conta(double saldo, double numConta, double numAgencia){
+      this.saldo = saldo;
+      this.numConta = numConta;
+      this.numAgencia = numAgencia;
     }
   
-    
+    // ideia de método: transferencia
   
     
   }
