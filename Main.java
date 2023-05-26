a352915695886865e2bba293ca1e97552678a553
Alessandro <alessandrogpf@hotmail.com>
2023-05-26 01:20:06 -0400

    Estou ajustando a Main

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
-public class main{
+import java.util.Scanner;
+public class Main{
     public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
         Cliente cliente = new Cliente(null, null, null, 0, null, null, null, null, null);
         Conta conta = new Conta(0, 0, 0);
         contaCorrente contaCorrente = new contaCorrente();
