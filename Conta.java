ec56df632818691e8103764dae1a6a5e63f22f4b
Alessandro <alessandrogpf@hotmail.com>
2023-05-30 15:08:00 -0400

    Fiz alterações na classe conta, deem uma olhada

--- Conta.java
+++ Conta.java
@@ -1,18 +1,45 @@
-public class Conta {
+public abstract class Conta {
     /* ATRIBUTOS */
-    private double saldo;
-    private double numConta;
-    private double numAgencia;
+    protected double saldo;
+    protected double numConta;
+    protected double numAgencia;
+    protected double limCredito;
     
   
     //construtor
-    public Conta(double saldo, double numConta, double numAgencia){
+    public Conta(double saldo, double numConta, double numAgencia, double limCredito){
       this.saldo = saldo;
       this.numConta = numConta;
       this.numAgencia = numAgencia;
+      this.limCredito = limCredito;
     }
   
     // ideia de método: transferencia
+  // abstract transferencia
+  // abstract deposito
+  // empréstimo
+  // limite do cartão de crédito
   
-    
+  
+  public double getSaldo(){
+    return saldo;
+  }
+  
+  public double getNumconta(){
+    return numConta;
+  }
+
+  public double getNumagencia(){
+    return numAgencia;
+  }
+  public double getLimcredito(){
+    return limCredito;
+  }
+  public double setLimcredito(double limCredito){
+    this.limCredito = limCredito;
+  }
+  
+  
+  // não coloquei nenhum setter porque creio que saldo, numConta e numAgencia não devem ser alterados pelo usuário
+  
   }
