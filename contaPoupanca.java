59e49e73399b4b6274f7ad37b3fa9c2946582bd8
Beatriz Veron Roa <91498661+Beatrizveron@users.noreply.github.com>
2023-05-31 16:07:38 -0400

    Update contaPoupanca.java

--- contaPoupanca.java
+++ contaPoupanca.java
@@ -1,10 +1,13 @@
 public class contaPoupanca extends Conta {
     /* Atributos */
-    private int jurosMensais; // Rendimento da poupança do mês
-  
-  
-  
-  
-  
-    
+  private double taxaRendimento; // Rendimento da poupança do mês
+
+  public contaPoupanca(double saldo, double numConta, double numAgencia, String nomeTitular, double taxaRendimento){
+    super(saldo, numConta, numAgencia, nomeTitular);
+    this.taxaRendimento = taxaRendimento;
   }
+
+  public double getTaxaRendimento(){
+    return taxaRendimento;
+  }
+}
