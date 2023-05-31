4a4e4ce6c8bc312fcf490bebccb278a0c10608e3
Beatriz Veron Roa <91498661+Beatrizveron@users.noreply.github.com>
2023-05-31 14:57:53 -0400

    Update Conta.java
    criei métodos e arrumei a classe

--- Conta.java
+++ Conta.java
@@ -1,38 +1,57 @@
-public abstract class Conta {
+abstract class Conta {
     /* ATRIBUTOS */
     protected double saldo;
     protected double numConta;
     protected double numAgencia;
-    
-    
+    protected String nomeTitular;
+    protected double limCredito;
   
     //construtor
-    public Conta(double saldo, double numConta, double numAgencia, double limCredito){
+    public Conta(double saldo, double numConta, double numAgencia, String nomeTitular, double limCredito){
       this.saldo = saldo;
       this.numConta = numConta;
       this.numAgencia = numAgencia;
+      this.nomeTitular = nomeTitular;
       this.limCredito = limCredito;
     }
   
-    // ideia de método: transferencia
-  // abstract transferencia
-  // abstract deposito
-  // empréstimo
-  // limite do cartão de crédito
-  
-  
-  public double getSaldo(){
-    return saldo;
-  }
-  
-  public double getNumconta(){
-    return numConta;
-  }
+    // abstract deposito
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+    
+    //transferencia
+    public boolean transferir(Conta destinatario, double valor){
+        if(valor <= saldo){
+            saldo -= valor;
+            destinatario.depositar(valor);
+            return true;
 
-  public double getNumagencia(){
-    return numAgencia;
-  }
-  
+        }else{
+            return false;
+        }
+    }
+    
+    // consultar saldo
+    public String getInfo() {
+        return "Seu saldo é de R$" +saldo;
+    }
+
+    public double getNumconta(){
+        return numConta;
+    }
+
+    public double getNumagencia(){
+        return numAgencia;
+    }
+
+    public String getNomeTitular(){
+        return nomeTitular;
+    }
+
+    public double getLimcredito(){
+        return limCredito;
+    }
   
   
   // não coloquei nenhum setter porque creio que saldo, numConta e numAgencia não devem ser alterados pelo usuário
