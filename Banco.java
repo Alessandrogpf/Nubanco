3ab16f3d9dae9d074d79466cdf222dd50897f7f8
Alessandro <alessandrogpf@hotmail.com>
2023-06-10 17:02:15 -0400

    Alterações em banco

--- Banco.java
+++ Banco.java
@@ -1,7 +1,47 @@
- /* Possível Classe para armazenar os clientes e cálculos do sistema */
- public class Banco { // Ou public class Sistema
-    private pessoaJuridica clientesJ[];
-    private pessoaFisica clientesF[];
+import java.util.*;
+
+public class Banco{
+  /*************
+  * ATRIBUTOS *
+  *************/
+	private String nome;
+	private List<clienteJuridico> ListJ;
+	private List<clienteFisico> ListF;
+
+	
+	/*CONSTRUTOR*/
+	public Banco (String nome){
+		this.nome = nome;
+		this.ListJ = new ArrayList<>();
+		this.ListF = new ArrayList<>();
+	}
+	
+  /***********
+  * MÉTODOS *
+  ***********/
+	public void addClienteFisico(clienteFisico clienteFisico) {
+		if (ListF.contains(clienteFisico)) {
+            System.out.println("Erro: O cliente já existe em nosso sistema");
+		}else {
+            ListF.add(clienteFisico);
+            System.out.println("Cliente foi adicionado com sucesso.");
+        }
+    }
+
+	public void addClienteJuridico(clienteJuridico clienteJuridico) {
+        if (ListJ.contains(clienteJuridico)) {
+            System.out.println("Erro: O cliente já existe em nosso sistema.");
+        } else {
+            ListJ.add(clienteJuridico);
+            System.out.println("Cliente foi adicionado com sucesso.");
+        }
+    }
   
-   
-  }
+    /********************* 
+     * SETTERS E GETTERS *
+    *********************/
+	public String getNome() {
+		return nome;
+	}
+  
+}
